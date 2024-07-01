2bd3b6b58a858592a641096cd807e4ceb850b6f7
zhus-dika <zhus.dinara@gmail.com>
2024-07-02 00:47:25 +0500

    edit main script

--- model_based_approach/main.py
+++ model_based_approach/main.py
@@ -21,8 +21,8 @@ args = dotdict({
     'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
     'checkpoint': './temp/',
-    'load_model': False,
-    'load_folder_file': ('models','checkpoint_3.pth.tar'),
+    'load_model': True,
+    'load_folder_file': ('models','checkpoint_2.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
